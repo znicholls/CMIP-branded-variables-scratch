fed8a2e0adbfa777d6d908feca7a383e45524fb4
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-03-03 15:43:14 +0100

    Sketch out testing todos

---
+++ tests/integration/test_branded_variable_name_generator.py
@@ -0,0 +1,10 @@
+"""
+Tests of our generation of branded variable names
+"""
+
+# Tests to write:
+# - Given a CMIP6 variable name and cell methods, do we get expected output
+# - Match the behaviour specified in the excel table
+# - Given a branded variable, get back to CMIP6 variable and cell methods
+# - Round tripping
+# - If you give an uncreognised name, you get a sensible error
