b687816bb13c39f4066208a020b7e65a7a74e64b
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2025-03-03 15:51:49 +0100

    A few demo examples

--- tests/integration/test_branded_variable_name_generator.py
+++ tests/integration/test_branded_variable_name_generator.py
@@ -8,3 +8,37 @@ Tests of our generation of branded variable names
 # - Given a branded variable, get back to CMIP6 variable and cell methods
 # - Round tripping
 # - If you give an uncreognised name, you get a sensible error
+
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    ["variable", "cell_methods", "exp", "expectation"],
+    (
+        pytest.param(
+            "tas", "time: mean area: mean", "tas-tavg-hx-u-u", id="tas-average"
+        ),
+        ("hfds", "time: mean area: sum", "hfds-tavg-hx-s-u"),
+        ("junk", "time: mean area: sum", "hfds-tavg-hx-s-u"),
+    ),
+)
+def test_basic(variable, cell_methods, exp):
+    res = get_branded_variable(variable=variable, cell_methods=cell_methods)
+
+    assert res == exp
+
+
+@pytest.mark.parametrize(
+    ["variable", "expectation"],
+    (
+        pytest.param("tas", does_not_raise()),
+        pytest.param(
+            "junk", pytest.raises(KeyError, match="some helpful error message here")
+        ),
+    ),
+)
+def test_unrecognised_variable_raises(variable, expectation):
+    with expectation:
+        get_branded_variable(variable=variable, cell_methods="not used")
